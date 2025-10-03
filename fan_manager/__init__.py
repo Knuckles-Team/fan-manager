2320deac33860ab5b53407b82539854702d7e48e
KnucklesSG1 <knucklessg1@gmail.com>
2025-10-03 01:26:38 -0500

    Import all funcs in init.

--- fan_manager/__init__.py
+++ fan_manager/__init__.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from fan_manager.fan_manager_mcp import fan_manager_mcp
-from fan_manager.fan_manager import (
-    fan_manager,
-    setup_logging,
-    get_core_temp,
-    get_temp,
-    set_fan,
-    auto_set_fan_speed,
-)
+import importlib
+import inspect
+
+# List of modules to import from
+MODULES = [
+    "fan_manager.fan_manager",
+    "fan_manager.fan_manager_mcp",
+]
+
+# Initialize __all__ to expose all public classes and functions
+__all__ = []
+
+# Dynamically import all classes and functions from the specified modules
+for module_name in MODULES:
+    module = importlib.import_module(module_name)
+    for name, obj in inspect.getmembers(module):
+        # Include only classes and functions, excluding private (starting with '_')
+        if (inspect.isclass(obj) or inspect.isfunction(obj)) and not name.startswith(
+            "_"
+        ):
+            globals()[name] = obj
+            __all__.append(name)
 
 """
 fan-manager
@@ -17,13 +30,3 @@ fan-manager
 Manager your Dell PowerEdge Fan Speed with this handy tool!
 Support MCP Server
 """
-
-__all__ = [
-    "fan_manager",
-    "fan_manager_mcp",
-    "setup_logging",
-    "get_core_temp",
-    "get_temp",
-    "set_fan",
-    "auto_set_fan_speed",
-]
